2e822abbf9f99fe11a86f2d70b9746a0211d4327
Patrick McAuliffe <patrickmcauliffe2011@u.northwestern.edu>
2024-02-01 15:52:28 +0000

    Finish security and passwords

--- blango/settings.py
+++ blango/settings.py
@@ -82,6 +82,12 @@ class Dev(Configuration):
 }
 
     # Application definition
+    PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.Argon2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
 
     INSTALLED_APPS = [
         'django.contrib.admin',
