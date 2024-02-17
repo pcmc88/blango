1bb99bb5125308aec83c198d8a575cb64fdd4aae
Patrick McAuliffe <patrickmcauliffe2011@u.northwestern.edu>
2024-02-17 22:31:33 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -190,6 +190,8 @@ class Dev(Configuration):
     # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    ACCOUNT_ACTIVATION_DAYS = 7
 
 
 
