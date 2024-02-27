3765999dfbcbb299c73287643248d1cc16fab49c
Patrick McAuliffe <patrickmcauliffe2011@u.northwestern.edu>
2024-02-27 17:15:01 +0000

    Finish permissions

--- blango/settings.py
+++ blango/settings.py
@@ -154,8 +154,12 @@ class Dev(Configuration):
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
-    ]
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+    ],
 }
+
 
 
 
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from blog.api.permissions import AuthorModifyOrReadOnly
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 
 from blog.api.serializers import PostSerializer
 from blog.models import Post
@@ -10,5 +12,6 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
